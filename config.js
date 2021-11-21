3718378a2ae32135993688b225def6cb67ee7fd6
RenB0T <94209612+RenB0T@users.noreply.github.com>
2021-11-21 21:58:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-global.owner = ['62895330379186'] // Put your number here
+global.owner = ['6285845823026'] // Put your number here
 global.mods = [] // Want some help?
-global.prems = ['62895330379186'] // Premium user has unlimited limit
+global.prems = ['6285845823026'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -18,8 +18,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = '@ainee_bot'
-global.author = 'Aine'
+global.packname = '@ainee_bot2'
+global.author = 'Aine2'
 
 global.multiplier = 69 // The higher, The harder levelup
 
